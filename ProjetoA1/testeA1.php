324a2d3cc562bc487f86dd3e98484635adadab03
Alexandre Quintiliano <alexandrequintili@outlook.com>
2022-02-23 05:32:21 -0300

    Alteração arquivo Excel

--- ProjetoA1/testeA1.php
+++ ProjetoA1/testeA1.php
@@ -3,7 +3,7 @@
     ini_set('max_execution_time', 0);
 
     require 'conexao.php';
-    $bancoA1 = 'C:/A1/itens.csv';
+    $bancoA1 = 'C:\Users\asas\Documents\GitHub\EngenhariaA1\TesteA1\ProjetoA1/itens.csv';
 
     if(file_exists($bancoA1)) {
         mysqli_query($conecta, "LOAD DATA INFILE '$bancoA1' INTO TABLE itens 
