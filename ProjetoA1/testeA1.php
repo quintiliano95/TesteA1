55a41ad145e78026788c55e97a34dbe73e764e89
Alexandre Quintiliano <alexandrequintili@outlook.com>
2022-02-24 05:28:33 -0300

    Comentários adicionados

--- ProjetoA1/testeA1.php
+++ ProjetoA1/testeA1.php
@@ -1,6 +1,6 @@
 <?php
 
-    ini_set('max_execution_time', 0);
+    ini_set('max_execution_time', 0);  // Não interromper o script PHP que está sendo executado.
 
     require 'conexao.php';
     $bancoA1 = 'C:/Users/asas/Documents/GitHub/EngenhariaA1/TesteA1/ProjetoA1/itens.csv';
@@ -9,7 +9,7 @@
         mysqli_query($conecta, "LOAD DATA INFILE '$bancoA1' INTO TABLE itens 
         FIELDS TERMINATED BY ';'
         LINES TERMINATED BY '\n'
-        IGNORE 1 ROWS");
+        IGNORE 1 ROWS"); // Carregando os dados no banco de dados.
     } 
     else {
         echo "Arquivo não encontrado!";
