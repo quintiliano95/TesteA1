9a3c4b93b55d7db8dc6d622188c561fd34c6952a
Alexandre Quintiliano <alexandrequintili@outlook.com>
2022-02-24 07:16:57 -0300

    Comandos MySQL e update no README

--- ProjetoA1/testeA1.php
+++ ProjetoA1/testeA1.php
@@ -3,7 +3,7 @@
     ini_set('max_execution_time', 0);  // Não interromper o script PHP que está sendo executado.
 
     require 'conexao.php';
-    $bancoA1 = 'C:/Users/asas/Documents/GitHub/EngenhariaA1/TesteA1/ProjetoA1/itens.csv';
+    $bancoA1 = 'C:/Users/asas/Documents/GitHub/EngenhariaA1/TesteA1/ProjetoA1/itens.xlsx'; //Diretório do Banco de Dados
 
     if(file_exists($bancoA1)) {
         mysqli_query($conecta, "LOAD DATA INFILE '$bancoA1' INTO TABLE itens 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Teste A1 Engenharia 
  
- 
+ Neste projeto foi criado um banco de dados através do recebimento de um arquivo .CSV e feitas as devidas conexões para acessá-lo.
+
+ Para executar o projeto, será necessário instalar os seguintes programas:
+
+- [Servidor XAMPP: Necessário para criar um servidor para o banco de dados local](https://www.apachefriends.org/pt_br/index.html)
